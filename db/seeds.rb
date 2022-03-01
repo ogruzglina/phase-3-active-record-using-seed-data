b17051d6653fb05bf504d7b7c324f5844f671331
Olha Gruzglina <olga.zhmura@gmail.com>
2022-03-01 11:24:19 -0500

    commit tree e77ecb8dc3c47d2af4844e34b59836ef8b9a71f6

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,17 @@
 # Add a console message so we can see output when the seed file runs
+puts "Seeding games"
+50.times do
+    Game.create( #Faker has random info for games so we create a loop for creating fake records
+        title:Faker::Game.title,
+        genre:Faker::Game.genre,
+        platform: Faker::Game.platform,
+        price: rand(0..60)
+    )
+end
+
+puts "Done seeding"
+
+# Game.create(title: "Breath of the Wild", platform: "Switch", genre: "Action-adventure", price: 60)
+# Game.create(title: "Final Fantasy VII", platform: "Playstation", genre: "RPG", price: 60)
+# Game.create(title: "Mario Kart", platform: "Switch", genre: "Racing", price: 60)
+# Game.create(title: "Candy Crush Saga", platform: "Mobile", genre: "Puzzle", price: 0)
